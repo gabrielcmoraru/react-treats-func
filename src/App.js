d8800932691f5cf0a2c9ceb6d6dce686a7b6a949
Gabriel C Moraru <gabrielcmoraru@gmail.com>
2018-07-27 19:14:19 +0100

    context api basic func

--- src/App.js
+++ src/App.js
@@ -15,11 +15,20 @@ class UserProvider extends Component {
     email: 'john@doe.com'
   }
 
+  logout = () => {
+    this.setState({
+      id: null,
+      name: '',
+      email: '',
+    })
+  }
+
   render() {
     return(
       <UserContext.Provider
         value={{
-          user: this.state
+          user: this.state,
+          logout: this.logout,
         }}
       >
         {this.props.children}
