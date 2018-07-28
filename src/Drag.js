b5879eeec536f22564245e5c126e60658d406e52
Gabriel C Moraru <gabrielcmoraru@gmail.com>
2018-07-28 02:08:06 +0100

    events based on gesture interface

--- src/Drag.js
+++ src/Drag.js
@@ -20,6 +20,16 @@ const CardContainer = styled(animated.div)`
 `;
 
 export default class Drag extends Component {
+
+  onUp = xDelta => () => {
+    console.log(xDelta)
+    if(xDelta < -300) {
+      alert('Remove Card');
+    } else if (xDelta > 300) {
+      alert('Accept Card');
+    }
+  }
+
   render() {
     return (
       <Gesture>
@@ -40,7 +50,10 @@ export default class Drag extends Component {
                             extrapolate: 'clamp',
                           })
             }}>
-              <DragCard style={{
+              <DragCard
+              onMouseUp={this.onUp(xDelta)}
+              onTouchEnd={this.onUp(xDelta)}
+              style={{
                 opacity: x.interpolate({
                           range: [-300, 20],
                           output: [0, 1],
