562458daf1c5b9a72914271d6f60058c3d2bed7f
Gabriel C Moraru <gabrielcmoraru@gmail.com>
2018-07-28 01:19:44 +0100

    animate draggable components

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Toggle } from 'Utilities';
 import { Modal, Card } from 'Elements';
 import User from './User';
 import UserProvider from './UserProvider';
-
+import Drag from './Drag';
 
 class App extends Component {
   render() {
@@ -21,19 +21,7 @@ class App extends Component {
               <h1>Still In Modal</h1>
             </Modal>
           </header>
-          <User />
-          <section>
-            <Toggle>
-              {({on, toggle}) => (
-                <Fragment>
-                  <button onClick={toggle}>Show ? Hide </button>
-                  <Transition from={{opacity: 0, bg: '#82d8d8', height: '0px'}} enter={{opacity: 1, bg: '#524763', height: '200px'}} leave={{opacity: 0, bg: '#82d8d8', height: '0px'}}>
-                    {on && Header}
-                  </Transition>
-                </Fragment>
-              )}
-            </Toggle>
-          </section>
+          <Drag />
           <Toggle>
             {({on, toggle}) => (
               <Fragment>
